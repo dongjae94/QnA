d89510484a65b5f4f9fdba48bf0cb4293ec863bf
dongjae94 <dongjae94@naver.com>
2020-05-14 18:02:42 +0900

    회원가입, URL 리팩토링 완료

--- src/main/java/com/signUp/UserController.java
+++ src/main/java/com/signUp/UserController.java
@@ -1,36 +1,33 @@
 package com.signUp;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.signUp.domain.User;
 import com.signUp.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users") // 시작위치
 public class UserController {
-	private List<User> users= new ArrayList<User>();
+	
 	
 	@Autowired
 	private UserRepository userRepository;
 	
-	@PostMapping("/user/create")
+	@PostMapping("") // users가 들어가면 회원가입-규
 	public String create(User user) {
-		System.out.print(user);
-		users.add(user);
 		userRepository.save(user);
-		return "redirect:/list"; // redirect =클라이언트로 돌아갔다가 다시 list메소드 실행
+		return "redirect:/users"; // redirect =클라이언트로 돌아갔다가 다시 list메소드 실행
 	}
 	
-	@GetMapping("/list") // redirect 응답
+	@GetMapping("") // redirect 응답
 	public String list(Model model) {
-		model.addAttribute("users", users);
-		return "list"; //templates - list.html로 정보 전
+		model.addAttribute("users", userRepository.findAll());
+		return "/user/list"; //    template - user/list.html로 정보 전
 	}
 }
  
