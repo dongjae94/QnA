f5f357c9d03d83dccd5917cb9fe1aa0f34cae786
dongjae94 <dongjae94@naver.com>
2020-05-19 18:56:27 +0900

    상세보기 기능 구현

--- src/main/java/com/signUp/QuestionController.java
+++ src/main/java/com/signUp/QuestionController.java
@@ -4,14 +4,14 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.signUp.domain.Question;
-import com.signUp.domain.QuestionRepository;
-//import com.signUp.domain.QuestionRepository;
-import com.signUp.domain.User;
+import com.signUp.domain.*;
+
 
 @Controller
 @RequestMapping("/questions")
@@ -21,12 +21,22 @@ public class QuestionController {
 	private QuestionRepository questionRepository; //spring에서 관리하는 repository를 사용하겠다는 의미
 	
 	@GetMapping("/form")
-	public String form(HttpSession session) {
+	public String form(HttpSession session, Question q) {
 		if(!HttpSessionUtils.isLoginUser(session)) {
 			return "/users/loginForm";
 		}
 		return "/qna/form";
 	}
+	
+	@GetMapping("/{id}")
+	public String show(HttpSession session, @PathVariable Long id, Model model) {
+		if(!HttpSessionUtils.isLoginUser(session)) {
+			return "/users/loginForm";
+		}
+		model.addAttribute("question", questionRepository.findById(id).get());
+		return "/qna/show";
+	}
+	
 	@PostMapping("")
 	public String create(String title, String contents, HttpSession session) {
 		if(!HttpSessionUtils.isLoginUser(session)) {
